907463a8394ac00195cca22f06154f8a474abb5e
Yan Fonseca <yanfonseca2002@gmail.com>
2023-10-05 11:12:36 -0300

    Adição de novas funcionalidades em Algorithm: Geração da lista de candidatos, heurística local e introdução da estrutura do construtivo

--- include/Algorithm.h
+++ include/Algorithm.h
@@ -12,6 +12,11 @@ class Algorithm {
     private:
         void constructive();
 
+        double localHeuristc(Node* actualNode, Node* b, double tripMaxTime);
+        bool sortCandidateListBasedOnBenefit(Node* a, Node* b);
+
+        std::vector<Node*> generateCandidateList(std::vector<Node*> trip, std::vector<Node*> hotels, std::vector<Node*> customers, int actual_trip);
+
         double calculateTimeBetweenNodes(Node* a, Node* b);
 
         Solution* initial_solution;
