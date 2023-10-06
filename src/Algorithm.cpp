995c7201c2a5fb8c22adb307e204a2440a79de96
Yan Fonseca <yanfonseca2002@gmail.com>
2023-10-06 11:17:41 -0300

    Estrutura do construtivo pronta. Necessário iniciar os testes

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -4,6 +4,7 @@
 
 Algorithm::Algorithm(Problem *data) {
     this->data = data; // dados do problema
+    this->_tripsLength = data->getTripsLength();
 
     Solution* solution = new Solution(data);
     this->initial_solution = solution; // solução inicial vazia
@@ -15,31 +16,50 @@ Algorithm::~Algorithm() {
 
 void Algorithm::constructive() {
     std::vector<std::vector<Node*>> solution = this->initial_solution->getTrips(); // lista vazia
+    double tripMaxTime;
 
     std::vector<Node*> hotels = this->data->getHotels();
     std::vector<Node*> customers = this->data->getCustomers();
-
-    int actual_trip = 0;
 
     // hotel H0 inserido na primeira trip
     solution[0].push_back(hotels[0]);
 
     // Irá rodar para cada trip
-    for(auto trip : solution) {
+     for(int actual_trip=0; actual_trip<solution.size(); actual_trip++) {
+        // Avaliar se já está na última trip
+        bool isLastTrip = ((solution.size() - actual_trip) == 1) ? 1 : 0;
+        // Se estiver na última trip irá considerar apenas o hotel H1 como hotel válido.
+        if(isLastTrip) {
+            hotels.erase(hotels.begin());
+            hotels.erase(hotels.begin() + 1, hotels.end());
+        }
 
         // Construir a lista inicial de candidatos e ordená-la
-        std::vector<Node*> CL = this->generateCandidateList(trip,hotels,customers,actual_trip);
+        std::vector<Node*> CL = this->generateCandidateList(solution[actual_trip],hotels,customers,actual_trip);
+
         // Enquanto houver candidatos válidos
         while(!CL.empty()) {
             // Selecionar um candidato da lista e adicioná-lo à trip
-            trip.push_back(CL[0]);
+            solution[actual_trip].push_back(CL[0]);
             // Retirar o candidato da lista
             CL.erase(CL.begin());
-            // Reconstruir a lista
-            CL = this->generateCandidateList(trip,hotels,CL,actual_trip);
-        }
+            
+            // Diminuir o tempo da trip
+            this->_tripsLength[actual_trip] -= this->calculateTimeBetweenNodes(solution[actual_trip][solution[actual_trip].size()-1],solution[actual_trip][solution[actual_trip].size()-2]);
 
+            // Reconstruir a lista
+            CL = this->generateCandidateList(solution[actual_trip],hotels,CL,actual_trip);
+        }
         // Se a lista estiver vazia, adicionar o Hotel mais próximo
+        for(auto hotel : hotels) {
+            if(tripMaxTime - this->calculateTimeBetweenNodes(solution[actual_trip][solution.size()-1],hotel) >= 0) {
+                solution[actual_trip].push_back(hotel);
+                if(actual_trip + 1 < solution.size()) {
+                    solution[actual_trip + 1].push_back(hotel);
+                }
+                break;
+            }
+        }
     }
 }
 
@@ -53,16 +73,7 @@ std::vector<Node*> Algorithm::generateCandidateList(std::vector<Node*> trip, std
     std::vector<Node*> CL; // Lista de candidatos vazia
 
     // Tempo máximo da trip atual
-    double tripMaxTime = this->data->getTripsLength()[actual_trip];
-
-    // Avaliar se já está na última trip
-    bool isLastTrip = ((this->data->getTripsAmount() - actual_trip) == 1) ? 1 : 0;
-
-    // Se estiver na última trip irá considerar apenas o hotel H1 como hotel válido.
-    if(isLastTrip) {
-        hotels.erase(hotels.begin());
-        hotels.erase(hotels.begin() + 1, hotels.end());
-    }
+    double tripMaxTime = this->_tripsLength[actual_trip];
 
     for(auto customer : customers) {
         // Para cada customer:
@@ -80,7 +91,8 @@ std::vector<Node*> Algorithm::generateCandidateList(std::vector<Node*> trip, std
     }
 
     // Realizar a odernação com base na heurística escolhida
-    std::sort(CL.begin(),CL.end(), sortCandidateListBasedOnBenefit);
+    if(!CL.empty())
+        std::sort(CL.begin(),CL.end(), sortCandidateListBasedOnBenefit);
 
     return CL;
 }
